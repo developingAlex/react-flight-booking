4261c2b912962b91e6c3a8dc378238b877e6284d
Alex Palma <alex-palma@outlook.com>
2017-11-27 11:23:47 +1100

    refactor Buttons.js code to use tidier syntax

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-function Button(props) {
+function Button({title, primary, magic}) {
   let className = 'btn'
 
-  if (props.primary){
+  if (primary){
     className += ' btn--primary'
   }
-  if (props.magic) {
+  if (magic) {
     className += ' btn--magic'
   }
   return (
     <button className={ className } >
-      { props.title }
+      { title }
       
     </button>
   )
